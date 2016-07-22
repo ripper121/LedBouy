1d761d0a42e0a46449e4995eac5ea8f9527fc706
ripper121 <sts@zwave.eu>
2016-07-22 16:18:49 +0200

    ...

--- Software/tiny85/tiny85.ino
+++ Software/tiny85/tiny85.ino
@@ -15,53 +15,56 @@
 //set timer to 4 sec
 //WDTCR |= (1<<WDP3) | (0<<WDP2) | (0<<WDP1) | (0<<WDP0);
 //set timer to 8 sec
-//WDTCR |= (1<<WDP3) | (0<<WDP2) | (0<<WDP1) | (0<<WDP1);
+//WDTCR |= (1<<WDP3) | (0<<WDP2) | (0<<WDP1) | (1<<WDP0);
 #include <avr/interrupt.h>
+#include <avr/power.h>
 #include <avr/sleep.h>
+#include <Vcc.h>
 byte power_count = 0;
+const float VccMin   = 2.0;           // Minimum expected Vcc level, in Volts.
+const float VccMax   = 3.0;           // Maximum expected Vcc level, in Volts.
+const float VccCorrection = 1.0 / 1.0; // Measured Vcc by multimeter divided by reported Vcc
+Vcc vcc(VccCorrection);
+
 ISR(WDT_vect) {
   power_count++;
 }
 
 void setup() {
-  WDTCR |= (1<<WDP3) | (0<<WDP2) | (0<<WDP1) | (0<<WDP1);
-
-  // Set watchdog timer in interrupt mode
-  WDTCR |= (1 << WDIE);
-  WDTCR |= (0 << WDE);
-
-  sei(); // Enable global interrupts
+  byte p = vcc.Read_Perc(VccMin, VccMax);
+  if (p > 100)
+    p = 100;
+  DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB4);
+  for (byte i = 0; i < p / 10; i++) {
+    PORTB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB4);
+    _delay_ms(250);
+    PORTB = 0x00;
+    _delay_ms(250);
+  }
+  // set timer to 8s
+  WDTCR |= (1 << WDP3) | (0 << WDP2) | (0 << WDP1) | (1 << WDP0) | (1 << WDIE) | (0 << WDE);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 }
 
 
 void loop() {
-  power_off();
+  //-----POWER OFF START-----
+  //PortB Input LOW
+  DDRB = 0x00;
+  PORTB = 0x00;
+  ADCSRA &= ~(1 << ADEN);
+  power_usi_disable();
+  power_adc_disable();
+  power_all_disable();
+  sleep_mode();
+  //-----POWER OFF END  -----
   if (power_count > 0) {
     power_count = 0;
-    pinMode(PB1, OUTPUT);
-    digitalWrite(PB1, HIGH);
-    delay(250);
+    DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB4);
+    PORTB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB4);
+    _delay_ms(100);
   }
 }
-
-void power_off() {
-  pinMode(PB0, INPUT);
-  pinMode(PB1, INPUT);
-  pinMode(PB2, INPUT);
-  pinMode(PB3, INPUT);
-  pinMode(PB4, INPUT);
-  digitalWrite(PB0, LOW);
-  digitalWrite(PB1, LOW);
-  digitalWrite(PB2, LOW);
-  digitalWrite(PB3, LOW);
-  digitalWrite(PB4, LOW);
-  ADCSRA &= ~(1 << ADEN);   //Turn off ADC
-  ACSR |= _BV(ACD);         //Disable analog comparator
-  sleep_mode();             //Go Sleep
-}
-
-
 
 
 
